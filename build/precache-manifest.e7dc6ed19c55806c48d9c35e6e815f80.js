2d671775f8d59ba4d8d75412351b8806027dffe8
Sibin Xavier <sibinx7@gmail.com>
2020-05-08 15:52:09 +0530

    Send production build folder to solve heroku issue

---
+++ build/precache-manifest.e7dc6ed19c55806c48d9c35e6e815f80.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b8ee3262157edaeeec2439f529b200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e19b2b4375346dc0b97",
+    "url": "/static/css/2.882f9545.chunk.css"
+  },
+  {
+    "revision": "29d8c0f45616e268656a",
+    "url": "/static/css/main.15c108e5.chunk.css"
+  },
+  {
+    "revision": "4e19b2b4375346dc0b97",
+    "url": "/static/js/2.24fa9729.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.24fa9729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d8c0f45616e268656a",
+    "url": "/static/js/main.9462bc9c.chunk.js"
+  },
+  {
+    "revision": "56d6721901a46534d52b",
+    "url": "/static/js/runtime-main.cbe7cef8.js"
+  },
+  {
+    "revision": "65d874119d159ab3ae8907e2f34f9206",
+    "url": "/static/media/btn-square-spl.65d87411.svg"
+  },
+  {
+    "revision": "28c9fd89eef52fd31c3aa95844d115fc",
+    "url": "/static/media/button-rect-spl.28c9fd89.svg"
+  },
+  {
+    "revision": "345b2ae44eee62167ea1e8141245080a",
+    "url": "/static/media/button-spl.345b2ae4.svg"
+  },
+  {
+    "revision": "fbe1bc31eaeba1e5157bf94882c55137",
+    "url": "/static/media/cec.fbe1bc31.svg"
+  },
+  {
+    "revision": "290fce0ca06e63933aa4270dd9bc1e65",
+    "url": "/static/media/chapter-1.290fce0c.jpg"
+  },
+  {
+    "revision": "e5cb97426959f9cbee26a4e9e5471583",
+    "url": "/static/media/chapter-2.e5cb9742.jpg"
+  },
+  {
+    "revision": "2606ab1a7f43301b86ce89be93a1a724",
+    "url": "/static/media/chapter-3.2606ab1a.jpg"
+  },
+  {
+    "revision": "b32e9670c496a5a6acdcf9cebc654e8f",
+    "url": "/static/media/coconut-cookies.b32e9670.ttf"
+  },
+  {
+    "revision": "47633bb588ce73433bf44ac9229a87d0",
+    "url": "/static/media/funny.47633bb5.ttf"
+  },
+  {
+    "revision": "60a2e2eeadcf83f5a5cbb7a92462c455",
+    "url": "/static/media/funny.60a2e2ee.woff"
+  },
+  {
+    "revision": "640559c1ef2ea8b93cbb2698d8736635",
+    "url": "/static/media/funny.640559c1.svg"
+  },
+  {
+    "revision": "edaa617324296ceb0b05ecbb5b992dcb",
+    "url": "/static/media/funny.edaa6173.eot"
+  },
+  {
+    "revision": "8b6c45d9d145b794de34b52dbeed1e47",
+    "url": "/static/media/global-home.8b6c45d9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ac809226efc5690d0afafd95223a65de",
+    "url": "/static/media/user.ac809226.svg"
+  }
+]);
